0f673c3fe52fc6ce4bdd8a840162f388a3e06619
nelcia27 <staszewskakornalia@gmail.com>
2020-06-14 14:53:08 +0200

    Finall commit

---
+++ Lab9/Lab9/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab9.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a genre is 100 characters!")]
+        public string Name { get; set; }
+
+        public ICollection<Song> SongsList { get; set; }
+    }
+}
---
+++ Lab9/Lab9/Models/Song.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab9.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Artist is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the artist is 100 characters!")]
+        public string Artist { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+
+
+    }
+}
